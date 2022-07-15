603adff0262aeba6bacafea94208a64a796efd03
Hamid <hamidtahir2000@gmail.com>
2022-07-15 10:32:17 +0500

    Final Github App Push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from 'Pages/Home';
-import About from './Pages/About';
-import NotFound from './Pages/NotFound';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Alert from './components/layout/Alert';
-import { GithubProvider } from './context/github/GithubContext';
-import { AlertProvider } from './context/alert/AlertContext';
-import User from './components/users/User';
+import About from 'Pages/About';
+import NotFound from 'Pages/NotFound';
+import Navbar from 'components/layout/Navbar';
+import Footer from 'components/layout/Footer';
+import Alert from 'components/layout/Alert';
+import { GithubProvider } from 'context/github/GithubContext';
+import { AlertProvider } from 'context/alert/AlertContext';
+import User from 'components/users/User';
 
 function App() {
   return (
