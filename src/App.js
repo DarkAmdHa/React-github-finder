4476065927be18356129f244d5ba79e6abbca29c
Hamid <hamidtahir2000@gmail.com>
2022-07-15 10:29:00 +0500

    Final Github App Push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Home from './Pages/Home';
+import Home from 'Pages/Home';
 import About from './Pages/About';
 import NotFound from './Pages/NotFound';
 import Navbar from './components/layout/Navbar';
